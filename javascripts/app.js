3d21c1654e87d3e8be41fed300915f1fb5e40516
Madeline Pickering <madelinepickering@gmail.com>
2016-06-01 15:43:57 -0600

    project images and about styling

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,22 @@
 $(function() {
-    $('img').on('click', function(){
-      var modal = '#'+($(this).attr('id'))+"Modal";
-      $(modal).show();
-    })
-    $('.close').on('click', function(){
-      $('.modal').hide();
-    })
+  $.fn.scrollView = function () {
+    return this.each(function () {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top
+        }, 1000);
+    });
+  }
+
+  $('.clickabout').on('click', function(event){
+    event.preventDefault();
+    $('#about').scrollView();
+  })
+
+  $('img').on('click', function(){
+    var modal = '#'+($(this).attr('id'))+"Modal";
+    $(modal).show();
+  })
+  $('.close').on('click', function(){
+    $('.modal').hide();
+  })
 });
